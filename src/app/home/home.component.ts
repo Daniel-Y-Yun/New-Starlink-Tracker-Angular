e7e5405bce83fed0c7d85ff242f4858631e17d59
Daniel Yun <57472020+Daniel-Y-Yun@users.noreply.github.com>
2023-10-20 15:39:38 -0400

    Testing api call to NORAD

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideMenuComponent } from '../side-menu/side-menu.component';
+import env  from '../../assets/json/env.json'; 
 
 @Component({
   selector: 'app-home',
@@ -12,11 +13,26 @@ import { SideMenuComponent } from '../side-menu/side-menu.component';
   template: `
     <div>
       <app-side-menu></app-side-menu>
+      {{ data }}
     </div>
   `,
   styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent {
+  url = env.env.base_api_url;
+  api_key = env.env.api_key;
+  data = [];
+  // `${baseUrl}tle/${satId}&apiKey=${apiKey}`
 
+  async getSatData() {
+    const data = await fetch('{this.url}tle/45730&apiKey={api_key}');
+    return await data.json() ?? [];
+  }
+
+  test() {
+    this.getSatData().then(data => {
+        this.data = data;
+      });
+  }
 }
